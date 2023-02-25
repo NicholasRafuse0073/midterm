5dc7c8ea6d07e58ab1b109eb0da22450779b48a7
NicholasRafuse0073 <nnick2420@gmail.com>
2023-02-24 20:31:27 -0500

    Spinner and array added

---
+++ app/src/main/java/nicholas/rafuse/n01440073/mid/ni/NickTopFrag.java
@@ -0,0 +1,30 @@
+package nicholas.rafuse.n01440073.mid.ni;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NickTopFrag extends Fragment {
+
+    public NickTopFrag() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_nick_top, container, false);
+    }
+}
---
+++ app/src/main/java/nicholas/rafuse/n01440073/mid/ni/RafuseBottomFrag.java
@@ -0,0 +1,29 @@
+package nicholas.rafuse.n01440073.mid.ni;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RafuseBottomFrag extends Fragment {
+
+
+    public RafuseBottomFrag() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_rafuse_bottom, container, false);
+    }
+}
