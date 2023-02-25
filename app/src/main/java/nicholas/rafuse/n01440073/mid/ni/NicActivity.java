13d967946c20e9c41d32197418015bd211353707
NicholasRafuse0073 <nnick2420@gmail.com>
2023-02-24 20:06:15 -0500

    Activity background coloured

--- app/src/main/java/nicholas/rafuse/n01440073/mid/ni/NicActivity.java
+++ app/src/main/java/nicholas/rafuse/n01440073/mid/ni/NicActivity.java
@@ -2,6 +2,7 @@ package nicholas.rafuse.n01440073.mid.ni;
 //Nicholas Rafuse, N01440073, CENG-258-0NB
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
@@ -11,5 +12,15 @@ public class NicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton imageButton = findViewById(R.id.nicImage);
+
+        imageButton.setOnClickListener(view -> openActivity());
     }
+
+    public void openActivity(){
+        Intent intent = new Intent(this, NicActivity2.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/nicholas/rafuse/n01440073/mid/ni/NicActivity2.java
@@ -0,0 +1,14 @@
+package nicholas.rafuse.n01440073.mid.ni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NicActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nic2);
+    }
+}
