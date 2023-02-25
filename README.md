7c769d6127de85a0cd573999b6a1365e9c4c5dc8
NicholasRafuse0073 <nnick2420@gmail.com>
2023-02-24 19:51:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Nicholas Rafuse, N01440073
+This is my midterm. I don't know what it does yet.
---
+++ app/src/main/java/nicholas/rafuse/n01440073/mid/ni/NicActivity.java
@@ -0,0 +1,15 @@
+package nicholas.rafuse.n01440073.mid.ni;
+//Nicholas Rafuse, N01440073, CENG-258-0NB
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class NicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
